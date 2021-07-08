ab67f15daf3935ee4b7ed78df394445da7bc2a14
no-brand <do.dream.david@gmail.com>
2021-07-08 22:54:28 +0900

    Create spring boot project

---
+++ init.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/zsh
+
+spring init -d=web,jpa,h2,thymeleaf -g=com.nobrand.journal -a=spring-boot-journal spring-boot-journal
---
+++ src/test/java/com/nobrand/journal/springbootjournal/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nobrand.journal.springbootjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
